6effc31f53d634d16ba632a158399ccacb23e50f
Yash Patel <99255356+Yash98305@users.noreply.github.com>
2024-03-19 11:37:58 +0530

    c4

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async()=>{
     try {
-        await mongoose.connect("mongodb://localhost:27017/E-Commerce",{family:4})
+        await mongoose.connect(process.env.MONGO_URL||"mongodb+srv://yash98305:yash98305@cluster0.gk4tfjv.mongodb.net/Art_Gallery",{family:4})
         console.log("mongodb connected.....")
     } catch (error) {
         console.log("error" + error.message)
